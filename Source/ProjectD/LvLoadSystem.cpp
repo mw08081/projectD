2a66ca17c649956ba11683571cddbb0e8c34e04b
mw08081 <president304@naver.com>
2024-06-21 17:27:36 +0900

    Added LvLoadSystem.cpp && Bug Fixed :ref destroyed spawnedMasterField in BP_Hana

---
+++ Source/ProjectD/LvLoadSystem.cpp
@@ -0,0 +1,22 @@
+﻿#include "LvLoadSystem.h"
+
+// 기본값 설정
+ALvLoadSystem::ALvLoadSystem()
+{
+	// 모든 프레임에서 Tick()을 호출하도록 이 actor을(를) 설정합니다. 필요하지 않은 경우 이 기능을 해제하여 성능을 향상시킬 수 있습니다.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// 게임이 시작되거나 생성될 때 호출됩니다.
+void ALvLoadSystem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// 매 프레임마다 호출됨
+void ALvLoadSystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Display, TEXT("hello world!"));
+}
---
+++ Source/ProjectD/LvLoadSystem.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LvLoadSystem.generated.h"
+
+UCLASS()
+class PROJECTD_API ALvLoadSystem : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// 이 actor의 속성에 대한 기본값을 설정합니다.
+	ALvLoadSystem();
+
+protected:
+	// 게임이 시작되거나 생성될 때 호출됩니다.
+	virtual void BeginPlay() override;
+
+public:
+	// 매 프레임마다 호출됨
+	virtual void Tick(float DeltaTime) override;
+};
