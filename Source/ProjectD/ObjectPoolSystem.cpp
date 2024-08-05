4f29220482da437ae807a68dcaac35667390ac33
mw08081 <president304@naver.com>
2024-08-05 21:49:19 +0900

    Modified Punch Logic, Added ObjectPoolSystem.h/cpp

---
+++ Source/ProjectD/ObjectPoolSystem.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ObjectPoolSystem.h"
+
+// Sets default values
+AObjectPoolSystem::AObjectPoolSystem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AObjectPoolSystem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AObjectPoolSystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
