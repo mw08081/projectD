e05b18ca49ba33caf5af1f094c87a316ffa00517
mw08081 <president304@naver.com>
2024-08-07 19:29:58 +0900

    Added Source/ProjectD/GameMode Directory

---
+++ Source/ProjectD/GameMode/DefaultGameMode.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DefaultGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTD_API ADefaultGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+    virtual void BeginPlay() override;
+
+private:
+    // The object pool
+    //UPROPERTY()
+    //class * ObjectPool;
+
+    // The class of the pooled actor
+    UPROPERTY(EditDefaultsOnly, Category = "Pooling")
+    TSubclassOf<AActor> PooledActorClass;
+
+    // The size of the pool
+    UPROPERTY(EditDefaultsOnly, Category = "Pooling")
+    int32 PoolSize;
+};
--- Source/ProjectD/ProjectD.Build.cs
+++ Source/ProjectD/ProjectD.Build.cs
@@ -12,20 +12,6 @@ public class ProjectD : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        //PrivateDependencyModuleNames.AddRange( new string[] { "Slate", "SlateCore", "EditorStyle", "UnrealEd" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-
-        // Editor 모듈 추가
-        //if (Target.bBuildEditor)
-        //{
-        //    PrivateDependencyModuleNames.Add("UnrealEd");
-        //}
+		//PrivateIncludePaths.Add("ProjectD");
     }
 }
