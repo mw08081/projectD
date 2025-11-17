60fd0f32aa78cf87c73e5e32b6e3c305df94549a
mw08081 <president304@naver.com>
2025-11-17 22:02:39 +0900

    Added FracturedPiece .h/.cpp

---
+++ Source/ProjectD/Actor/FracturedPiece.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FracturedPiece.generated.h"
+
+UCLASS()
+class PROJECTD_API AFracturedPiece : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFracturedPiece();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,Category = "Component")
+	TObjectPtr<class UStaticMeshComponent> Mesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Component")
+	TObjectPtr<class ULvObjectRoot> LvObjectRoot;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+};
